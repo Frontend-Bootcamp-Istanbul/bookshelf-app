5434eae1164b0bcb3e9457301168bc4f37b1a6b6
Bengi <bengihazal@gmail.com>
2020-05-15 01:34:29 +0300

    Update BookStyles.js

--- src/components/Homepage/BookStyles.js
+++ src/components/Homepage/BookStyles.js
@@ -2,8 +2,12 @@ import styled from "styled-components";
 import {Card} from "reactstrap";
 
 export const StyledBookCard = styled(Card)`
-        height: 444px;
+        height: 530px;
         .card-img-top {
             height: 250px;
         }
+        width:250px;
+        margin-top:20px;
+        margin-left: 20px;
+        margin-bottom: 20px; 
 `;
